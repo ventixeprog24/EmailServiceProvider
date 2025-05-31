c369cc5c58a7d4b73e62aae1975001113cf4e919
emil hult <Emilhult@live.com>
2025-05-31 18:12:31 +0200

    Tests

---
+++ EmailServiceProvider.Tests/EmailResultReplyFactory_Tests.cs
@@ -0,0 +1,38 @@
+﻿using EmailServiceProvider.Dtos;
+using EmailServiceProvider.Factories;
+
+namespace EmailServiceProvider.Tests
+{
+    public class EmailResultReplyFactory_Tests
+    {
+        [Fact]
+        public void CreateSuccessReply_ShouldReturnSuccessReply()
+        {
+            // Arrange
+            var factory = new EmailResultReplyFactory();
+            var result = new EmailResultDto { IsSuccess = true, Result = "Email sent successfully." };
+
+            // Act
+            var reply = factory.CreateSucceesReply(result);
+
+            // Assert
+            Assert.True(reply.Succeeded);
+            Assert.Equal("Email sent successfully.", reply.Result);
+        }
+
+        [Fact]
+        public void CreateFailedReply_ShouldReturnFailedReply()
+        {
+            // Arrange
+            var factory = new EmailResultReplyFactory();
+            var result = new EmailResultDto { IsSuccess = false, Result = "Email sending failed." };
+
+            // Act
+            var reply = factory.CreateFailedReply(result);
+
+            // Assert
+            Assert.False(reply.Succeeded);
+            Assert.Equal("Email sending failed.", reply.Result);
+        }
+    }
+}
